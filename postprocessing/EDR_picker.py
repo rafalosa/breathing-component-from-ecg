613f056477f91595bde9a10b3959f6b3667a34a4
rafalosa <rafal.osad@gmail.com>
2022-04-10 23:56:39 +0200

    Prepared basis for a module test + minor fix

--- postprocessing/EDR_picker.py
+++ postprocessing/EDR_picker.py
@@ -3,8 +3,11 @@ from scipy.signal import welch
 from scipy.interpolate import splev, splrep
 from scipy.integrate import simps
 from typing import Tuple
+import wfdb.io
 import matplotlib.pyplot as plt
 
+
+# todo: Wrap it.
 
 def sort_EDR(candidates: np.ndarray, time_domain: np.ndarray, window_width: float, sampling_frequency: float) \
         -> Tuple[np.ndarray, np.ndarray]:
@@ -36,9 +39,13 @@ def sort_EDR(candidates: np.ndarray, time_domain: np.ndarray, window_width: floa
         max_index = np.argmax(EDR_spectrum)  # Locate global maximum
 
         # Place window at global maximum with window_width
-        inside_range = np.r_[max_index - window_width_index // 2: max_index + window_width_index // 2 + 1]
-        outside_range = np.r_[0:max_index - window_width_index // 2,
-                              max_index + window_width_index // 2 + 1: EDR_spectrum.shape[0]]
+
+        left_bracket = int(max_index - window_width_index // 2)
+        right_bracket = int(max_index + window_width_index // 2 + 1)
+
+        inside_range = np.r_[left_bracket: right_bracket]
+        outside_range = np.r_[0:left_bracket,
+                              right_bracket: EDR_spectrum.shape[0]]
 
         EDR_spectrum_inside = EDR_spectrum[inside_range]
         EDR_spectrum_domain_inside = EDR_spectrum_domain[inside_range]
@@ -55,15 +62,40 @@ def sort_EDR(candidates: np.ndarray, time_domain: np.ndarray, window_width: floa
         EDRS.append(interpolated_EDR)
 
     # Sort EDR candidates with ascending order with respect to the fractions
-    complement = zip(fractions, EDRS)
-    sorted_complements = sorted(complement, reverse=False)
+    zipped_results = zip(fractions, EDRS)
+    sorted_results = sorted(zipped_results, reverse=False)
 
-    sorted_fractions = np.array([el[0] for el in sorted_complements])
-    sorted_EDR = np.array([el[1] for el in sorted_complements])
+    sorted_fractions = np.array([el[0] for el in sorted_results])
+    sorted_EDR = np.array([el[1] for el in sorted_results])
 
     return sorted_fractions, sorted_EDR
 
 
 if __name__ == "__main__":
 
-    pass
+    ann = wfdb.io.rdann("../data/fantasia_wfdb/f1y05", "ecg")
+    data = wfdb.io.rdrecord("../data/fantasia_wfdb/f1y05")
+
+    respiration = data.p_signal[:,0]
+    idxs = ann.sample
+
+    pt_len = 180
+    start = 700
+
+    chosen = idxs[start:start + pt_len]
+    chosen_time = np.array(chosen) * 1/250
+
+    respiration_chosen_raw = np.array(respiration[chosen])
+
+    fig = plt.figure(figsize=(20, 5))
+
+    fig.add_subplot(2, 1, 1)
+    plt.plot(respiration_chosen_raw)
+
+    frac, edr = sort_EDR(np.array([respiration_chosen_raw]), chosen_time, .08, 250)
+
+    fig.add_subplot(2, 1, 2)
+    plt.plot(edr[0])
+    print(frac)
+
+    plt.show()
