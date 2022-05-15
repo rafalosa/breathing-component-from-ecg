000023c929b03e6a6a7818691469a897f9f17416
rafalosa <rafal.osad@gmail.com>
2022-05-15 16:24:10 +0200

    Plotter update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from scipy.signal import savgol_filter
+from scipy.signal import savgol_filter, welch
 import numpy as np
+import matplotlib.pyplot as plt
 
 from data_processing import ECGPreprocessor, EDRExtractor, EDRPicker
 from tools.plot import CollectivePlotter
@@ -8,6 +9,7 @@ from tools.signal_testing import coherence, cross_correlation
 START_TIME = 2251  # [s]
 SIGNAL_LENGTH = 300  # [s]
 RANDOM_STATE = 69
+PLOTS_NUM = 3
 
 DB_PATH = 'data/fantasia_wfdb/f1o01'
 ecg_preprocessor = ECGPreprocessor(DB_PATH,
@@ -43,14 +45,14 @@ ica_spectra = picker.power_spectra
 edr_pca = picker2.apply(pca_edr, r_timestamps, method="sugpen", crop_to_freq=.6)
 pca_spectra = picker2.power_spectra
 
-pl = CollectivePlotter(6, 4)
+pl = CollectivePlotter(3, 4)
 
 edr_domain = np.arange(r_timestamps[0], r_timestamps[-1], 1/SAMPLING_FREQ)
 
-pl.set_col_values(0, edr_ica[:6])
-pl.set_col_values(2, edr_pca[:6])
-pl.set_col_values(1, [values[1] for values in ica_spectra[:6]])
-pl.set_col_values(3, [values[1] for values in pca_spectra[:6]])
+pl.set_col_values(0, edr_ica[:PLOTS_NUM])
+pl.set_col_values(2, edr_pca[:PLOTS_NUM])
+pl.set_col_values(1, [values[1] for values in ica_spectra[:PLOTS_NUM]])
+pl.set_col_values(3, [values[1] for values in pca_spectra[:PLOTS_NUM]])
 
 pl.set_col_domain(0, edr_domain)
 pl.set_col_domain(2, edr_domain)
@@ -64,7 +66,6 @@ pl.set_col_xlabel(0, "Time (s)")
 pl.set_col_xlabel(1, "Frequency (Hz)")
 pl.set_col_xlabel(2, "Time (s)")
 pl.set_col_xlabel(3, "Frequency (Hz)")
-
 pl.show()
 
 # Test section
@@ -81,3 +82,17 @@ ica_coherence = coherence(resp_signal, edr_ica_to_test, fs=SAMPLING_FREQ, window
 
 print(f'PCA cross correlation and coherence results: {pca_cross_corr:.2f}, {pca_coherence:.2f}')
 print(f'ICA cross correlation and coherence results: {ica_cross_corr:.2f}, {ica_coherence:.2f}')
+
+pl2 = CollectivePlotter(1, 2)
+
+pl2.set_col_values(0, [savgol_filter(ecg_preprocessor.respiration_signal, 150, 2)])
+pl2.set_col_domain(0, np.linspace(START_TIME, START_TIME+SIGNAL_LENGTH, SIGNAL_LENGTH*SAMPLING_FREQ))
+pl2.set_col_boundaries(0, (START_TIME, START_TIME + 100))
+
+domain, respiration_spectrum = welch(ecg_preprocessor.respiration_signal, fs=SAMPLING_FREQ, nperseg=2 ** 13, nfft=2 ** 16)
+pl2.set_col_values(1, [respiration_spectrum])
+pl2.set_col_domain(1, domain)
+pl2.set_col_boundaries(1, (0, .6))
+
+pl2.show()
+
